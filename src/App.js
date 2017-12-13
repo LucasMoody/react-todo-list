1e26a4207507e190dd7194ac559525c9a9f8a468
Lucas Pradel <lucaspradel@gmail.com>
2017-12-13 16:18:16 +0100

    Manual todo list items

--- src/App.js
+++ src/App.js
@@ -7,8 +7,21 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">VANTAiO Todo Liste</h1>
         </header>
-        <div className="content">
-
+        <div className="App-content">
+            <div className="todo-list">
+                <div className="todo-list-item">
+                    Aufräumen
+                </div>
+                <div className="todo-list-item">
+                    Kochen
+                </div>
+                <div className="todo-list-item">
+                    Einkaufen
+                </div>
+                <div className="todo-list-item">
+                    Paket abholen
+                </div>
+            </div>
         </div>
       </div>
     );
