aceff4c0cd4ae2a37045789aba490789afb4cc76
Lucas Pradel <lucaspradel@gmail.com>
2017-12-19 10:34:52 +0100

    Added checkbox to complete todo task

--- src/TodoList.js
+++ src/TodoList.js
@@ -7,17 +7,40 @@ class TodoList extends Component {
         super(props);
         this.state = {
             items: [
-                'Aufräumen',
-                'Kochen',
-                'Einkaufen',
-                'Paket abholen'
+                {
+                    name: 'Aufräumen',
+                    done: false
+                },
+                {
+                    name: 'Kochen',
+                    done: false
+                },
+                {
+                    name: 'Einkaufen',
+                    done: false
+                },
+                {
+                    name: 'Paket abholen',
+                    done: false
+                }
             ]
         }
     }
 
+    onItemClicked = (idx, done) => {
+        const newItems = [].concat(this.state.items);
+        newItems[idx].done = done;
+        this.setState({items: newItems});
+    }
+
     onEnter = e => {
         if(e.key === 'Enter') {
-            this.setState({items: this.state.items.concat(e.target.value)});
+            this.setState({
+                items: this.state.items.concat({
+                    name: e.target.value,
+                    done: false
+                }
+            )});
             e.target.value = '';
         }
     }
@@ -26,7 +49,7 @@ class TodoList extends Component {
         return (
             <div className="todo-list">
                 <input type="text" style={{width: '100%'}} onKeyPress={this.onEnter}/>
-                {this.state.items.map((item, idx) => <TodoListItem key={idx} name={item}/>)}
+                {this.state.items.map((item, idx) => <TodoListItem key={idx} name={item.name} done={item.done} onItemClicked={this.onItemClicked.bind(this, idx)}/>)}
             </div>
         );
     }
--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 class TodoListItem extends Component {
-  render() {
-    return (
-        <div className="todo-list-item">
-            {this.props.name}
-        </div>
-    );
-  }
+
+    onCheckboxChanged = e => {
+        this.props.onItemClicked(e.target.checked);
+    }
+
+    render() {
+        return (
+            <div className="todo-list-item">
+                <input type="checkbox" checked={this.props.done} onChange={this.onCheckboxChanged}/>
+                <span style={{textDecoration: this.props.done ? 'line-through' : 'initial'}}>{this.props.name}</span>
+            </div>
+        );
+    }
 }
 
 export default TodoListItem;
