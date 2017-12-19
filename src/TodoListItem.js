50eccb5a30c44c7f97d855c56c6c7b553ec26c98
Lucas Pradel <lucaspradel@gmail.com>
2017-12-19 15:57:59 +0100

    Converted list items into semantic ui components

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import { Checkbox } from 'semantic-ui-react';
 
 class TodoListItem extends Component {
 
-    onCheckboxChanged = e => {
-        this.props.onItemClicked(e.target.checked);
+    onCheckboxChanged = (e, data) => {
+        this.props.onItemClicked(data.checked);
     }
 
     render() {
         return (
-            <div className="todo-list-item">
-                <input type="checkbox" checked={this.props.done} onChange={this.onCheckboxChanged}/>
-                <span style={{textDecoration: this.props.done ? 'line-through' : 'initial'}}>{this.props.name}</span>
+            <div className="todo-list-item" style={{display: 'flex'}}>
+                <Checkbox checked={this.props.done} onChange={this.onCheckboxChanged}/>
+                <span style={{marginLeft: '10px', textDecoration: this.props.done ? 'line-through' : 'initial'}}>{this.props.name}</span>
             </div>
         );
     }
